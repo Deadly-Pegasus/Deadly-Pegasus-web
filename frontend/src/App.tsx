fd45b247e676bd4cdb2811587b28bf186a66a207
Laura Arockiasamy <laanov30@gmail.com>
2025-10-11 18:22:49 -0400

    Order Feature

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,7 @@ import './App.css';
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import Home from './Home/Home';
 import Products from './Products/Products';
+import Orders from './Orders/Orders'
 
 function App() {
 
@@ -27,6 +28,7 @@ const closeMenu = () => {
                 <div className="header-links">
                     <Link to="/">Home</Link>
                     <Link to="/catalog">Catalog</Link>
+                    <Link to="/orders">Orders</Link>
                 </div>
             </header>
 
@@ -50,6 +52,8 @@ const closeMenu = () => {
         <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/catalog" element={<Products />} />
+            <Route path="orders" element={<Orders />}></Route>
+            <Route path="/order/:id" element={<OrderDetail />}></Route>
         </Routes>
     </main>
     <footer className="footer">
